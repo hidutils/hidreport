e30a401bf9600aeb4d893f67d85a1e389c296da1
Peter Hutterer <peter.hutterer@who-t.net>
2024-05-18 15:29:18 +1000

    Add tests for all hid report descriptors in wacom-hid-descriptors
    These are all the report descriptors in the
    https://github.com/linuxwacom/wacom-hid-descriptors
    as of commit 490ce1ccc1767531d269dac9f4d562425f22661a.
    
    Copy those as data files and try to parse each of them to make sure
    we're resilient enough for that.

---
+++ build.rs
@@ -0,0 +1,35 @@
+use std::path::PathBuf;
+use std::io::Write;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // For each hid.bin file in our test/data directory, create one basic test function
+    // that attempts to parse that report descriptor
+    let datadir: PathBuf = [concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data")].iter().collect();
+    let out_dir = std::env::var_os("OUT_DIR").unwrap();
+    let dest_path = PathBuf::from(&out_dir).join("test-report-descriptors.rs");
+    let mut file = std::fs::File::create(dest_path)?;
+
+    writeln!(file, "use hidreport::*;")?;
+    writeln!(file)?;
+
+    std::fs::read_dir(datadir)
+        .unwrap()
+        .flatten()
+        .for_each(|rdesc| {
+            let filename = rdesc.file_name().into_string().unwrap();
+            let funcname = filename.replace([':', '.'], "_");
+            let path = rdesc.path();
+            writeln!(file, "
+#[test]
+#[allow(non_snake_case)]
+fn test_{funcname}() {{
+    let bytes: Vec<u8> = std::fs::read({path:?}).unwrap();
+    if !bytes.is_empty() {{
+        ReportDescriptor::try_from(&bytes).expect(&format!(\"Failed to parse {filename}\"));
+    }}
+}}
+").unwrap();
+        });
+
+    Ok(())
+}
