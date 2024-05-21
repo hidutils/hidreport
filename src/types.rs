3e86da2eefb48ebba56da4ab7894ce2e66c582eb
Peter Hutterer <peter.hutterer@who-t.net>
2024-05-22 09:32:32 +1000

    Fix the Units hid parsing handling
    A complete misunderstanding on my side of the table on Page 37 - fix
    this and add the various conversions, tests and even a Display for the
    units.

--- src/types.rs
+++ src/types.rs
@@ -6,6 +6,8 @@
 //! In this document and unless stated otherwise, a reference to "Section a.b.c" refers to the
 //! [HID Device Class Definition for HID 1.11](https://www.usb.org/document-library/device-class-definition-hid-111).
 
+use crate::TwosComplement;
+
 /// Creates a `From<Foo> for u32` and `From<u32> for Foo` implementation for the given `Foo` type.
 /// Use like this: `impl_from(Foo, Foo, u32)`.
 macro_rules! impl_from {
@@ -91,27 +93,127 @@ pub enum UnitSystem {
     EnglishRotation,
 }
 
-#[derive(Debug, Clone, Copy)]
+impl From<UnitSystem> for u32 {
+    fn from(system: UnitSystem) -> u32 {
+        match system {
+            UnitSystem::None => 0,
+            UnitSystem::SILinear => 1,
+            UnitSystem::SIRotation => 2,
+            UnitSystem::EnglishLinear => 3,
+            UnitSystem::EnglishRotation => 4,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Units {
     None,
-    Centimeter,
-    Radians,
-    Inch,
-    Degrees,
-    Gram,
-    Slug,
-    Seconds,
-    Kelvin,
-    Fahrenheit,
-    Ampere,
-    Candela,
+    Centimeter { exponent: i8 },
+    Radians { exponent: i8 },
+    Inch { exponent: i8 },
+    Degrees { exponent: i8 },
+    Gram { exponent: i8 },
+    Slug { exponent: i8 },
+    Seconds { exponent: i8 },
+    Kelvin { exponent: i8 },
+    Fahrenheit { exponent: i8 },
+    Ampere { exponent: i8 },
+    Candela { exponent: i8 },
+}
+
+impl std::fmt::Display for Units {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let (unit, exp) = match self {
+            Units::None => ("", &0i8),
+            Units::Centimeter { exponent } => ("cm", exponent),
+            Units::Radians { exponent } => ("rad", exponent),
+            Units::Inch { exponent } => ("in", exponent),
+            Units::Degrees { exponent } => ("deg", exponent),
+            Units::Gram { exponent } => ("g", exponent),
+            Units::Slug { exponent } => ("slug", exponent),
+            Units::Seconds { exponent } => ("s", exponent),
+            Units::Kelvin { exponent } => ("K", exponent),
+            Units::Fahrenheit { exponent } => ("F", exponent),
+            Units::Ampere { exponent } => ("A", exponent),
+            Units::Candela { exponent } => ("cd", exponent),
+        };
+
+        // quietly clamp to 4 bit range
+        let (unit, exp) = match exp {
+            0 => ("", 0),
+            exp @ -8..=7 => (unit, *exp),
+            _ => ("", 0),
+        };
+
+        // Superscripts: 0 is skipped altogether and 1 is left out for superscripts
+        const SUP: [&str; 16] = [
+            "⁻⁸", "⁻⁷", "⁻⁶", "⁻⁵", "⁻⁴", "⁻³", "⁻²", "⁻¹", "", "", "²", "³", "⁴", "⁵", "⁶", "⁷",
+        ];
+        let exp = SUP[(exp + 8) as usize];
+        write!(f, "{unit}{exp}")
+    }
+}
+
+impl From<Units> for u32 {
+    /// Convert from a unit into the byte representation of that unit.
+    /// The [UnitSystem] is set only for [Units::Centimeter], [Units::Radians],
+    /// [Units::Inch] and [Units::Degrees] where it is non-ambiguous,
+    /// for all other values the system is [UnitSystem::None] and needs to
+    /// be manually added by the caller:
+    ///
+    /// ```
+    /// # use hidreport::types::*;
+    /// let v = u32::from(Units::Centimeter { exponent: -2 });
+    /// let unit = Unit::from(v);
+    /// assert!(matches!(unit.length(), Units::Centimeter { exponent: -2 }));
+    /// // Centimeters are non-ambiguous
+    /// assert!(matches!(unit.system(), UnitSystem::SILinear));
+    ///
+    /// let v = u32::from(Units::Seconds { exponent: 3 });
+    /// let unit = Unit::from(v);
+    /// assert!(matches!(unit.time(), Units::Seconds { exponent: 3 }));
+    /// // Seconds are ambiguous
+    /// assert!(matches!(unit.system(), UnitSystem::None));
+    ///
+    /// // Explicitly set a system
+    /// let v = u32::from(UnitSystem::SILinear) | v;
+    /// let unit = Unit::from(v);
+    /// assert!(matches!(unit.time(), Units::Seconds { exponent: 3 }));
+    /// assert!(matches!(unit.system(), UnitSystem::SILinear));
+    /// ```
+    ///
+    /// Any exponent outside the 4-bit twos complement range is quietly ignored
+    /// and set to zero.
+    fn from(u: Units) -> u32 {
+        let (system, nibble, exponent) = match u {
+            Units::None => (UnitSystem::None, 0, 0),
+            Units::Centimeter { exponent } => (UnitSystem::SILinear, 1, exponent),
+            Units::Radians { exponent } => (UnitSystem::SIRotation, 1, exponent),
+            Units::Inch { exponent } => (UnitSystem::EnglishLinear, 1, exponent),
+            Units::Degrees { exponent } => (UnitSystem::EnglishRotation, 1, exponent),
+            Units::Gram { exponent } => (UnitSystem::None, 2, exponent),
+            Units::Slug { exponent } => (UnitSystem::None, 2, exponent),
+            Units::Seconds { exponent } => (UnitSystem::None, 3, exponent),
+            Units::Kelvin { exponent } => (UnitSystem::None, 4, exponent),
+            Units::Fahrenheit { exponent } => (UnitSystem::None, 4, exponent),
+            Units::Ampere { exponent } => (UnitSystem::None, 5, exponent),
+            Units::Candela { exponent } => (UnitSystem::None, 6, exponent),
+        };
+
+        let exp: u32 = match exponent {
+            exp @ 0..=7 => exp as u32,
+            exp @ -8..=-1 => (16 + exp) as u32,
+            _ => 0u32,
+        };
+
+        u32::from(system) | (exp << (nibble * 4))
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
 pub struct Unit(pub(crate) u32);
 
 impl_from!(Unit, Unit, u32);
-impl_fmt!(Unit, u32);
 
 impl Unit {
     fn nibbles(&self) -> Vec<u8> {
@@ -153,77 +255,125 @@ impl Unit {
             Some(2) => UnitSystem::SIRotation,
             Some(3) => UnitSystem::EnglishLinear,
             Some(4) => UnitSystem::EnglishRotation,
-            Some(n) => panic!("invalid size {n} for a nibble"),
+            Some(n) => panic!("invalid size {n} for the UnitSystem nibble"),
         }
     }
 
     /// The length, one of [Units::Centimeter], [Units::Radians], [Units::Inch]
-    /// or [Units::Degrees].
+    /// or [Units::Degrees] depending on the [UnitSystem].
     /// Returns [Units::None] if unset.
     pub fn length(&self) -> Units {
         match self.nibbles().get(1) {
             None | Some(0) => Units::None,
-            Some(1) => Units::Centimeter,
-            Some(2) => Units::Radians,
-            Some(3) => Units::Inch,
-            Some(4) => Units::Degrees,
-            Some(n) => panic!("invalid size {n} for a nibble"),
+            Some(n) => {
+                let exponent = n.twos_comp(4);
+                match self.system() {
+                    UnitSystem::None => Units::None,
+                    UnitSystem::SILinear => Units::Centimeter { exponent },
+                    UnitSystem::SIRotation => Units::Radians { exponent },
+                    UnitSystem::EnglishLinear => Units::Inch { exponent },
+                    UnitSystem::EnglishRotation => Units::Degrees { exponent },
+                }
+            }
         }
     }
 
-    /// The mass, one of [Units::Gram] or [Units::Slug].
+    /// The mass, one of [Units::Gram] or [Units::Slug] depending
+    /// on the [UnitSystem].
     /// Returns [Units::None] if unset.
     pub fn mass(&self) -> Units {
         match self.nibbles().get(2) {
             None | Some(0) => Units::None,
-            Some(1) => Units::Gram,
-            Some(2) => Units::Gram,
-            Some(3) => Units::Slug,
-            Some(4) => Units::Slug,
-            Some(n) => panic!("invalid size {n} for a nibble"),
+            Some(n) => {
+                let exponent = n.twos_comp(4);
+                match self.system() {
+                    UnitSystem::None => Units::None,
+                    UnitSystem::SILinear => Units::Gram { exponent },
+                    UnitSystem::SIRotation => Units::Gram { exponent },
+                    UnitSystem::EnglishLinear => Units::Slug { exponent },
+                    UnitSystem::EnglishRotation => Units::Slug { exponent },
+                }
+            }
         }
     }
 
     /// The time, one of [Units::Seconds].
     /// Returns [Units::None] if unset.
+    ///
+    /// This unit is not [UnitSystem] dependent and will return
+    /// the right value regardless of the [UnitSystem] set.
     pub fn time(&self) -> Units {
-        match self.nibbles().get(2) {
+        match self.nibbles().get(3) {
             None | Some(0) => Units::None,
-            Some(1..=4) => Units::Seconds,
-            Some(n) => panic!("invalid size {n} for a nibble"),
+            Some(n) => {
+                let exponent = n.twos_comp(4);
+                Units::Seconds { exponent }
+            }
         }
     }
 
-    /// The time, one of [Units::Kelvin] or [Units::Fahrenheit].
+    /// The time, one of [Units::Kelvin] or [Units::Fahrenheit],
+    /// depending on the [UnitSystem].
     /// Returns [Units::None] if unset.
+    ///
+    /// This unit is not [UnitSystem] dependent and will return
+    /// the right value regardless of the [UnitSystem] set.
     pub fn temperature(&self) -> Units {
-        match self.nibbles().get(3) {
+        match self.nibbles().get(4) {
             None | Some(0) => Units::None,
-            Some(1) => Units::Kelvin,
-            Some(2) => Units::Kelvin,
-            Some(3) => Units::Fahrenheit,
-            Some(4) => Units::Fahrenheit,
-            Some(n) => panic!("invalid size {n} for a nibble"),
+            Some(n) => {
+                let exponent = n.twos_comp(4);
+                match self.system() {
+                    UnitSystem::None => Units::None,
+                    UnitSystem::SILinear => Units::Kelvin { exponent },
+                    UnitSystem::SIRotation => Units::Kelvin { exponent },
+                    UnitSystem::EnglishLinear => Units::Fahrenheit { exponent },
+                    UnitSystem::EnglishRotation => Units::Fahrenheit { exponent },
+                }
+            }
         }
     }
 
     /// The current, one of [Units::Ampere].
+    ///
+    /// This unit is not [UnitSystem] dependent and will return
+    /// the right value regardless of the [UnitSystem] set.
     pub fn current(&self) -> Units {
-        match self.nibbles().get(3) {
+        match self.nibbles().get(5) {
             None | Some(0) => Units::None,
-            Some(1..=4) => Units::Ampere,
-            Some(n) => panic!("invalid size {n} for a nibble"),
+            Some(n) => {
+                let exponent = n.twos_comp(4);
+                Units::Ampere { exponent }
+            }
         }
     }
 
     /// The current, one of [Units::Candela].
     /// Returns [Units::None] if unset.
+    ///
+    /// This unit is not [UnitSystem] dependent and will return
+    /// the right value regardless of the [UnitSystem] set.
     pub fn luminosity(&self) -> Units {
-        match self.nibbles().get(3) {
+        match self.nibbles().get(6) {
             None | Some(0) => Units::None,
-            Some(1..=4) => Units::Candela,
-            Some(n) => panic!("invalid size {n} for a nibble"),
+            Some(n) => {
+                let exponent = n.twos_comp(4);
+                Units::Candela { exponent }
+            }
         }
+    }
+}
+
+impl std::fmt::Display for Unit {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let units = self
+            .units()
+            .unwrap_or_default()
+            .iter()
+            .map(|u| format!("{u}"))
+            .collect::<Vec<String>>()
+            .join("");
+        write!(f, "{units}")
     }
 }
 
@@ -358,3 +508,120 @@ pub struct Delimiter(pub(crate) u32);
 
 impl_from!(Delimiter, Delimiter, u32);
 impl_fmt!(Delimiter, u32);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_units() {
+        for nibble in 1..=6 {
+            // SILinear and SIRotation
+            for system in 1..=2 {
+                let exp = 2;
+                let v = system | exp << (4 * nibble);
+                let u = Unit::from(v);
+                match system {
+                    1 => assert!(matches!(u.system(), UnitSystem::SILinear)),
+                    2 => assert!(matches!(u.system(), UnitSystem::SIRotation)),
+                    _ => panic!("Invalid system value"),
+                };
+                match nibble {
+                    1 => match system {
+                        0x1 => assert!(matches!(u.length(), Units::Centimeter { exponent: 2 })),
+                        0x2 => assert!(matches!(u.length(), Units::Radians { exponent: 2 })),
+                        _ => panic!("Invalid system value"),
+                    },
+                    2 => assert!(matches!(u.mass(), Units::Gram { exponent: 2 })),
+                    3 => assert!(matches!(u.time(), Units::Seconds { exponent: 2 })),
+                    4 => assert!(matches!(u.temperature(), Units::Kelvin { exponent: 2 })),
+                    5 => assert!(matches!(u.current(), Units::Ampere { exponent: 2 })),
+                    6 => assert!(matches!(u.luminosity(), Units::Candela { exponent: 2 })),
+                    _ => panic!("Invalid nibble value"),
+                }
+            }
+
+            // EnglishLinear and EnglishRotation
+            for system in 3..=4 {
+                let exp = 0xd; // two's comp for -3
+                let v = system | exp << (4 * nibble);
+                let u = Unit::from(v);
+                match system {
+                    3 => assert!(matches!(u.system(), UnitSystem::EnglishLinear)),
+                    4 => assert!(matches!(u.system(), UnitSystem::EnglishRotation)),
+                    _ => panic!("Invalid system value"),
+                };
+                match nibble {
+                    1 => match system {
+                        0x3 => assert!(matches!(u.length(), Units::Inch { exponent: -3 })),
+                        0x4 => assert!(matches!(u.length(), Units::Degrees { exponent: -3 })),
+                        _ => panic!("Invalid system value"),
+                    },
+                    2 => assert!(matches!(u.mass(), Units::Slug { exponent: -3 })),
+                    3 => assert!(matches!(u.time(), Units::Seconds { exponent: -3 })),
+                    4 => assert!(matches!(
+                        u.temperature(),
+                        Units::Fahrenheit { exponent: -3 }
+                    )),
+                    5 => assert!(matches!(u.current(), Units::Ampere { exponent: -3 })),
+                    6 => assert!(matches!(u.luminosity(), Units::Candela { exponent: -3 })),
+                    _ => panic!("Invalid nibble value"),
+                }
+            }
+        }
+
+        // The various examples from Section 6.2.2.7, page 39
+        // Energy
+        let u = Unit::from(0xE121);
+        assert!(matches!(u.system(), UnitSystem::SILinear));
+        assert!(matches!(u.length(), Units::Centimeter { exponent: 2 }));
+        assert!(matches!(u.mass(), Units::Gram { exponent: 1 }));
+        assert!(matches!(u.time(), Units::Seconds { exponent: -2 }));
+        assert!(matches!(u.current(), Units::None));
+        assert!(matches!(u.luminosity(), Units::None));
+        assert!(matches!(u.temperature(), Units::None));
+        assert_eq!(format!("{u}"), "cm²gs⁻²");
+
+        // Angular Acceleration
+        let u = Unit::from(0xE012);
+        assert!(matches!(u.system(), UnitSystem::SIRotation));
+        assert!(matches!(u.length(), Units::Radians { exponent: 1 }));
+        assert!(matches!(u.mass(), Units::None));
+        assert!(matches!(u.time(), Units::Seconds { exponent: -2 }));
+        assert!(matches!(u.current(), Units::None));
+        assert!(matches!(u.temperature(), Units::None));
+        assert!(matches!(u.luminosity(), Units::None));
+        assert_eq!(format!("{u}"), "rads⁻²");
+
+        // Voltage
+        let u = Unit::from(0x00F0D121);
+        assert!(matches!(u.system(), UnitSystem::SILinear));
+        assert!(matches!(u.length(), Units::Centimeter { exponent: 2 }));
+        assert!(matches!(u.mass(), Units::Gram { exponent: 1 }));
+        assert!(matches!(u.time(), Units::Seconds { exponent: -3 }));
+        assert!(matches!(u.current(), Units::Ampere { exponent: -1 }));
+        assert!(matches!(u.temperature(), Units::None));
+        assert!(matches!(u.luminosity(), Units::None));
+        assert_eq!(format!("{u}"), "cm²gs⁻³A⁻¹");
+    }
+
+    #[test]
+    fn test_unit_conversion() {
+        for exp in 1..=15 {
+            for nibble in 1..=6 {
+                let v = 0x1u32 | (exp << (nibble * 4)); // SILinear
+                let unit = Unit::from(v);
+                assert_eq!(u32::from(unit), v);
+
+                // Compose the values from the units() vector
+                let uval = u32::from(unit.system())
+                    | unit
+                        .units()
+                        .unwrap()
+                        .iter()
+                        .fold(0, |acc, u| acc | u32::from(*u));
+                assert_eq!(uval, v);
+            }
+        }
+    }
+}
