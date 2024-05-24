hidutils/hidreport

commit 814e1bb5fbbfa02c6a19c4ac4dd9fabbdace9093

lib: if the logical/physical minimum is negative, reinterpret the max
Our maximums default to unsigned but if we have signed min/max
re-interpret the maximum as signed data.

src/lib.rs
@@ -1390,12 +1390,45 @@ fn parse_report_descriptor(bytes: &[u8]) -> Result<ReportDescriptor> {
                 update_stack!(stack, globals, logical_minimum, minimum);
             }
             ItemType::Global(GlobalItem::LogicalMaximum { maximum }) => {
+                // We don't know if the maximum is signed or unsigned unless we
+                // look at the minimum value and check if that is signed or unsigned.
+                // We default to signed but if the minimum is unsigned, we might have
+                // to re-interpret.
+                let minimum = stack
+                    .globals_const()
+                    .logical_minimum
+                    .unwrap_or(LogicalMinimum(0));
+                let mut maximum = maximum;
+                if minimum < LogicalMinimum(0) {
+                    if let Some(data) = item.data() {
+                        if data.len() > 0 {
+                            maximum = LogicalMaximum(hid::hiddata_signed(&data).unwrap());
+                        }
+                    }
+                };
+                println!("setting to {maximum}");
                 update_stack!(stack, globals, logical_maximum, maximum);
             }
             ItemType::Global(GlobalItem::PhysicalMinimum { minimum }) => {
                 update_stack!(stack, globals, physical_minimum, minimum);
             }
             ItemType::Global(GlobalItem::PhysicalMaximum { maximum }) => {
+                // We don't know if the maximum is signed or unsigned unless we
+                // look at the minimum value and check if that is signed or unsigned.
+                // We default to signed but if the minimum is unsigned, we might have
+                // to re-interpret.
+                let minimum = stack
+                    .globals_const()
+                    .physical_minimum
+                    .unwrap_or(PhysicalMinimum(0));
+                let mut maximum = maximum;
+                if minimum < PhysicalMinimum(0) {
+                    if let Some(data) = item.data() {
+                        if data.len() > 0 {
+                            maximum = PhysicalMaximum(hid::hiddata_signed(&data).unwrap())
+                        }
+                    }
+                };
                 update_stack!(stack, globals, physical_maximum, maximum);
             }
             ItemType::Global(GlobalItem::UnitExponent { exponent }) => {

src/hid.rs
@@ -40,7 +40,7 @@ fn bit(bits: u32, bit: u8) -> bool {
 }
 
 /// Convenience function to extract the bytes into a u32
-fn hiddata(bytes: &[u8]) -> Option<u32> {
+pub(crate) fn hiddata(bytes: &[u8]) -> Option<u32> {
     match bytes.len() {
         0 => None,
         1 => Some(bytes[0] as u32),
@@ -51,7 +51,7 @@ fn hiddata(bytes: &[u8]) -> Option<u32> {
 }
 
 /// Convenience function to extract the bytes into a i32
-fn hiddata_signed(bytes: &[u8]) -> Option<i32> {
+pub(crate) fn hiddata_signed(bytes: &[u8]) -> Option<i32> {
     match bytes.len() {
         0 => None,
         1 => Some((bytes[0] as i8) as i32),

commit adc2ff26a83adf3145ca1e3cc86a2b5806e164a4

Remove leftover debug message
Fixes: 814e1bb5fbbf ("lib: if the logical/physical minimum is negative, reinterpret the max")

src/lib.rs
@@ -1406,7 +1406,6 @@ fn parse_report_descriptor(bytes: &[u8]) -> Result<ReportDescriptor> {
                         }
                     }
                 };
-                println!("setting to {maximum}");
                 update_stack!(stack, globals, logical_maximum, maximum);
             }
             ItemType::Global(GlobalItem::PhysicalMinimum { minimum }) => {